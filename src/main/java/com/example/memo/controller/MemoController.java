29bd11be92929b97b25196d5e90aa03964af95a3
minkyungCHO <mk981015@naver.com>
2025-02-03 23:58:53 +0900

    feat: 메모 조회

--- src/main/java/com/example/memo/controller/MemoController.java
+++ src/main/java/com/example/memo/controller/MemoController.java
@@ -28,4 +28,12 @@ public class MemoController {
 
         return new MemoResponseDto(memo);
     }
+
+    @GetMapping("/{id}")
+    public MemoResponseDto findMemoById(@PathVariable Long id) {
+
+        Memo memo = memoList.get(id);
+
+        return new MemoResponseDto(memo);
+    }
 }
